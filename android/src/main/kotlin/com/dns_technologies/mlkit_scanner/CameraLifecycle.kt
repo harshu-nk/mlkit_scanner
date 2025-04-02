62151eb08c47d8cfc4f3f237e9ebb270b0bce789
Harshana Nishshanka <hrs_n62@yahoo.com>
2025-04-02 23:25:09 +0530

    Update CameraLifecycle.kt

--- android/src/main/kotlin/com/dns_technologies/mlkit_scanner/CameraLifecycle.kt
+++ android/src/main/kotlin/com/dns_technologies/mlkit_scanner/CameraLifecycle.kt
@@ -5,29 +5,30 @@ import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LifecycleRegistry
 
 /**
- * Custom [LifecycleOwner] realisation.
+ * Custom [LifecycleOwner] realization.
  *
  * This implementation is needed to manage the camera lifecycle. This is necessary because
  * the [Lifecycle] provided by the parent Activity does not imply the ability to control
  * the camera based on the lifecycle of the flutter components
  */
 class CameraLifecycle : LifecycleOwner {
-    private var lifecycleRegistry: LifecycleRegistry = LifecycleRegistry(this)
+    private val lifecycleRegistry = LifecycleRegistry(this)
 
     init {
         lifecycleRegistry.currentState = Lifecycle.State.CREATED
     }
 
-    override fun getLifecycle() = lifecycleRegistry
+    override val lifecycle: Lifecycle
+        get() = lifecycleRegistry
 
-    /** Invoke after initialisation or pause camera */
+    /** Invoke after initialization or to resume the camera */
     fun resume() {
         lifecycleRegistry.currentState = Lifecycle.State.RESUMED
     }
 
     /**
-     * It should be called in cases when the camera is not currently required to be displayed,
-     * but it also does not require clearing the camera resources
+     * Should be called when the camera is not currently needed,
+     * but also does not require releasing camera resources
      */
     fun pause() {
         lifecycleRegistry.currentState = Lifecycle.State.CREATED
